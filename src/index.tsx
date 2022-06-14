972756075ba3ffe21f16a83ebf8b6b5ee4b86b80
yushurygina <yushurygina@phoenixit.ru>
2022-06-14 17:13:23 +0300

    update import

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "components/App";
 
 import "./assets/styles/index.css";
-import Test from "components/Test";
-
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
