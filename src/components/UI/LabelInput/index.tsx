73f18135f520d53ad47ec9a49fd451b7ca2df19d
Shurygina Julia <65460474+Shurygina-Jul@users.noreply.github.com>
2022-06-15 19:12:25 +0300

    Test2 (#1)
    * first
    
    * second
    
    * first+second
    
    * исправлена валидация формы
    
    Co-authored-by: yushurygina <yushurygina@phoenixit.ru>

--- src/components/UI/LabelInput/index.tsx
+++ src/components/UI/LabelInput/index.tsx
@@ -3,14 +3,13 @@ import clsx from "clsx";
 import { ILabelInput } from "./interface";
 
 function LabelInput(props: ILabelInput) {
-  const { label, className, children, errors, require } = props;
+  const { label, className, children, errors } = props;
 
   return (
     <label className={clsx("text-xl", className)}>
       {label && <p>{label}</p>}
-
       {children}
-      {require && errors && <div className="text-red">This field is required</div>}
+      {errors && <p className="text-red">Ошибка: Необходмио заполнить поле</p>}
     </label>
   );
 }
