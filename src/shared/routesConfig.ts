3930d5962cdc164b73ef41449def107392c8df5b
Shurygina Julia <melam.2916@gmail.com>
2023-02-21 15:44:00 +0300

    fix: routerConfig

--- src/shared/routesConfig.ts
+++ src/shared/routesConfig.ts
@@ -2,6 +2,6 @@ import CurrentTask from "pages/CurrentTask";
 import Main from "pages/Main";
 
 export const routesConfig = [
-  { path: "/", element: Main },
+  { path: "/amazing", element: Main },
   { path: "/task/:id", element: CurrentTask },
 ];
